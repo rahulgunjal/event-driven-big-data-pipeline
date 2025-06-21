f98fbf18ada83341b4f8afc5e010eb804d11163d
RahulGunjal <rahul161gunjal@gmail.com>
2025-06-21 16:51:35 +0530

    Weather Alert Producer Consumer Added

---
+++ kafka-producer/src/main/scala/WeatherbitProducer.scala
@@ -0,0 +1,43 @@
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord, ProducerConfig}
+import java.util.Properties
+import scala.io.Source
+import java.net.URL
+import scala.concurrent.duration._
+import scala.concurrent.Await
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object WeatherbitProducer {
+  def main(args: Array[String]): Unit = {
+
+    val props = new Properties()
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
+
+    val producer = new KafkaProducer[String, String](props)
+    val topic = "weather"
+
+    val apiKey = "a4d79bbcea674e5e92da1e6d4b8c74b3" // 👈 Replace this
+    val city = "Aurangabad"
+    val country = "IN"
+    val url = s"https://api.weatherbit.io/v2.0/current?city=$city&country=$country&key=$apiKey"
+
+    println(s"Starting Weatherbit Producer for $city, $country")
+
+    try {
+      val json = Source.fromURL(new URL(url)).mkString
+      println(s"Fetched weather data: $json")
+
+      val record = new ProducerRecord[String, String](topic, null, json)
+      producer.send(record)
+      println(s"Sent to Kafka topic '$topic'")
+
+    } catch {
+      case e: Exception => e.printStackTrace()
+    } finally {
+      producer.close()
+      println("Producer closed.")
+    }
+  }
+}
---
+++ spark-consumer/src/main/scala/WeatherAlertConsumer.scala
@@ -0,0 +1,80 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{Dataset, Row}
+
+object WeatherAlertConsumer {
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder
+      .appName("WeatherAlertConsumer")
+      .master("local[*]")
+      .getOrCreate()
+
+    import spark.implicits._
+
+    spark.sparkContext.setLogLevel("WARN")
+
+    // JSON schema
+    val schema = new StructType()
+      .add("data", ArrayType(
+        new StructType()
+          .add("temp", DoubleType)
+          .add("rh", DoubleType)
+          .add("wind_spd", DoubleType)
+          .add("precip", DoubleType)
+          .add("city_name", StringType)
+          .add("datetime", StringType)
+      ))
+
+    val df = spark.readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("subscribe", "weather")
+      .option("startingOffsets", "latest")
+      .load()
+
+    val jsonDF = df.selectExpr("CAST(value AS STRING)").as[String]
+
+    val weatherDF = jsonDF.select(from_json(col("value"), schema).alias("json"))
+      .selectExpr(
+        "json.data[0].temp as temp",
+        "json.data[0].rh as humidity",
+        "json.data[0].wind_spd as wind_spd",
+        "json.data[0].precip as precip",
+        "json.data[0].city_name as city",
+        "json.data[0].datetime as datetime"
+      )
+
+    val alertsDF = weatherDF.withColumn("alert",
+      when(col("temp") > 40, concat(lit("Heatwave detected in "), col("city")))
+        .when(col("temp") < 5, concat(lit("Frost warning in "), col("city")))
+        .when(col("wind_spd") > 50, concat(lit("Storm warning in "), col("city")))
+        .when(col("precip") > 10, concat(lit("Heavy rain in "), col("city")))
+        .otherwise(lit(null))
+    )
+
+    val finalAlerts = alertsDF.filter(col("alert").isNotNull)
+      .select("datetime", "city", "temp", "humidity", "wind_spd", "precip", "alert")
+
+
+    val query = finalAlerts.writeStream
+      .foreachBatch { (batchDF: Dataset[Row], batchId: Long) =>
+        if (batchDF.isEmpty) {
+          println(s"=== Batch $batchId: No alerts triggered. Weather is normal. ===")
+        } else {
+          println(s"=== Batch $batchId: Alerts triggered! ===")
+          batchDF.show(false)
+
+          batchDF.write
+            .mode("append")
+            .option("header", "true")
+            .csv("alerts_output")
+        }
+      }
+      .outputMode("append")
+      .start()
+
+    query.awaitTermination()
+  }
+}
